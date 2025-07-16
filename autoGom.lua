c2c7d159681bc8d427c45ab70e72f48845be27eb
lytrihangit <63119929+lytrihangit@users.noreply.github.com>
2025-07-17 06:17:02 +0700

    Create autoGom.lua

---
+++ autoGom.lua
@@ -0,0 +1,128 @@
+repeat wait() until game:IsLoaded()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+local GameEvents = ReplicatedStorage:WaitForChild("GameEvents")
+local BuyPetEgg = GameEvents:WaitForChild("BuyPetEgg")
+local Sell_Inventory = GameEvents:WaitForChild("Sell_Inventory")
+local PetEggService = GameEvents:WaitForChild("PetEggService")
+local DinoMachineService = GameEvents:WaitForChild("DinoMachineService_RE")
+
+local DataStream = GameEvents:WaitForChild("DataStream")
+
+local LocalPlayer = Players.LocalPlayer
+local Backpack = LocalPlayer:WaitForChild("Backpack")
+local Leaderstats = LocalPlayer:WaitForChild("leaderstats")
+local Sheckles = Leaderstats:WaitForChild("Sheckles")
+local fileName = LocalPlayer.Name .. ".json"
+
+local dataService = require(ReplicatedStorage.Modules:WaitForChild("DataService"))
+
+local listEggs = {"Dinosaur Egg", "Bug Egg", "Paradise Egg", "Mythical Egg", "Bee Egg"}
+local listPet = {"T-Rex", "Brontosaurus", "Red Fox", "Dragonfly", "Queen Bee", "Mimic Octopus"}
+
+local function writeData(request, text)
+	writefile(fileName, HttpService:JSONEncode({ Request = request, Text = text }))
+end
+
+local function getCharacter()
+	return LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+end
+
+local function getEggs()
+	local eggs = {}
+
+	for _, tool in ipairs(Backpack:GetChildren()) do
+		local name = tool.Name
+		if name:find("Egg") then
+			local eggName, qty = name:match("^(.-) x(%d+)$")
+			eggs[eggName or name] = tonumber(qty) or 1
+		end
+	end
+
+	return eggs
+end
+
+local function getFarm()
+	local Farms = workspace:WaitForChild("Farm"):GetChildren()
+
+	for _, Farm in ipairs(Farms) do
+		local Important = Farm:FindFirstChild("Important")
+		local Data = Important:FindFirstChild("Data")
+		local Owner = Data:FindFirstChild("Owner").Value
+
+		if Owner == LocalPlayer.Name then
+			return Farm
+		end
+	end
+end
+
+local function getRandomFarmPoint(locations)
+	local plots = locations:GetChildren()
+	if #plots == 0 then return Vector3.new(0, 4, 0) end
+
+	local plot = plots[math.random(#plots)]
+	local pivot, size = plot:GetPivot(), plot.Size
+	local x = math.random(math.ceil(pivot.X - size.X / 2), math.floor(pivot.X + size.X / 2))
+	local z = math.random(math.ceil(pivot.Z - size.Z / 2), math.floor(pivot.Z + size.Z / 2))
+
+	return Vector3.new(x, 4, z)
+end
+
+local function getEggFarms()
+    local farm = getFarm()
+    local objects = farm.Important.Objects_Physical:GetChildren()
+
+    return farm, objects
+end
+
+local function hatchPets()
+    local farm, obj = getEggFarms()
+
+    -- hatch all pet time hatch = 0
+    if #obj > 0 then
+        for _, egg in ipairs(obj) do
+            if egg:GetAttribute("OBJECT_TYPE") == "PetEgg" and egg:GetAttribute("TimeToHatch") == 0 then
+                PetEggService:FireServer("HatchPet", egg)
+            end
+            task.wait(0.5)
+        end
+    end
+
+    -- plant all egg in list from inventory
+	local locations = farm.Important:FindFirstChild("Plant_Locations")
+    for _, name in ipairs(listEggs) do
+        for _, tool in ipairs(Backpack:GetChildren()) do
+            if tool.Name:find(name) then
+                tool.Parent = getCharacter()
+
+                for _ = 1, 8 do
+                    PetEggService:FireServer("CreateEgg", getRandomFarmPoint(locations))
+                end
+
+                task.wait(0.5)
+            end
+        end
+    end
+end
+
+task.spawn(function ()
+    local success, err = pcall(function ()
+        while true do
+            writeData("", "Working...")
+
+            hatchPets()
+
+            writeData("Completed", HttpService:JSONEncode(getEggs()))
+
+            task.wait(10)
+        end
+    end)
+
+    if not success then
+        print("Error : " .. err)
+        writeData("", err)
+    end
+end)
